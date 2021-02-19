fa48229272b15700ccc1a3d37265b224bbd0af1f
irahul <rindhane@users.noreply.github.com>
2021-02-19 19:58:00 +0530

    regex order creator in mergePdf

--- scriptsAutomation/mergePdfs.py
+++ scriptsAutomation/mergePdfs.py
@@ -1,6 +1,7 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import os 
 import argparse
+import re
 
 #argparse definitions
 parser = argparse.ArgumentParser(description='Merge Individual pdfs within a folder to a single file')
@@ -9,6 +10,8 @@ parser.add_argument('--files_folder', type=str, default='./pdfs',
                         
 parser.add_argument('--final_file',metavar='f', type=str, default='./resultant.pdf',
                         help='path to save final merged file or default is  is ./resultant.pdf')
+parser.add_argument('--order_merge',metavar='o', type=str, default=None,
+                        help='if ordering regex is defined in the function within the program')
 
 args=parser.parse_args()
 
@@ -17,13 +20,29 @@ def get_individual_file_from_walk (root):
         for file_ in files:
             yield path+'/'+file_
 
+def regexSorting(reg,string):
+	return int(re.findall(re.compile(reg),string)[0])
 
-def main(path1=args.files_folder,path2=args.final_file):
+def order_the_files(path):
+	reg='\d+'
+	file_iterator=get_individual_file_from_walk(path)
+	seq=dict()
+	files=list()
+	for file_ in file_iterator:
+		seq[file_]=regexSorting(reg,file_)
+		files.append(file_)
+	return sorted(files, key=lambda x : seq[x])  
+
+
+def main(path1=args.files_folder,path2=args.final_file,order=args.order_merge):
     #initiate path variable 
     individual_pdfs_folder_path= path1
     path_final_pdf= path2
     # get individual pdfs from the path specified
-    files=get_individual_file_from_walk(individual_pdfs_folder_path)
+    if order :
+        files=order_the_files(individual_pdfs_folder_path)
+    else:
+        files=get_individual_file_from_walk(individual_pdfs_folder_path)
     # Call the PdfFileMerger
     mergedObject = PdfFileMerger() 
     # Write all the files into a file which is named as shown below
