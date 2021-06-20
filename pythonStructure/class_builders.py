95c8ff945c7b47482809bac445f50fad120077ab
irahul <rindhane@users.noreply.github.com>
2021-06-20 23:08:24 +0530

    improve: enhanced builder class for python

--- pythonStructure/class_builders.py
+++ pythonStructure/class_builders.py
@@ -6,3 +6,20 @@ class self_setup_class :
     def set_inputs(self,**inputs):
         for key in inputs:
             setattr(self,key,inputs.get(key))
+    def __getitem__(self,tag):
+        if not isinstance(tag,str):
+            raise ValueError (f"{tag} is not string , provide key in string format" )
+        return getattr(self,tag,None)
+    def __setitem__(self,tag,value):
+        if not isinstance(tag,str):
+            raise ValueError (f"{tag} is not string , provide key in string format" )
+        setattr(self,tag,value)
+        return value
+    def keys(self):
+        return list(vars(self).keys())
+    def items(self):
+        return list(vars(self).items())
+    def get(self,key,default=None):
+        if self.__getitem__(key) is None :
+            return default
+        return self.__getitem__(key)
