11260389e338127fddb9c915d14570fac8d188dd
irahul <rindhane@users.noreply.github.com>
2024-02-04 22:09:29 +0530

    refactor : encryption module for github_codes

--- cryptography/encryption.py
+++ cryptography/encryption.py
@@ -8,38 +8,70 @@ import os
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import sys
 
-password=input("provide the password :").encode()
-#salt=os.urandom(16)
-salt2=b'\x17JA]C{\x0f2\xecIi\x0c>\xaei\x16'
-kdf=PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt2,
-    iterations=10**7,
-)
-key=base64.urlsafe_b64encode(kdf.derive(password))
-f=Fernet(key)
-tmp=input('enter 1 for encryption or enter 2  for decryption :')
-if tmp=='1':
-    #help for multiline: https://stackoverflow.com/questions/30239092/how-to-get-multiline-input-from-user
-    print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it. \n")
-    contents = []
-    while True:
-        try:
-            line = input()
-        except EOFError:
-            break
-        contents.append(line)
-    message = '\n'.join(contents)
-    token=f.encrypt(message.encode())
-    print('\n')
-    print('encrypted bytes>> \n')
-    print(token)
-if tmp =='2':
-    message=input('enter all bytes displayed as string \n')
-    token=f.decrypt(message.encode())
-    print('\n resultant text \n')
-    print(token.decode())
 
-print('\n\n process is complete \n\n')
+def main(fileName=None) : 
+    password=input("provide the password :").encode()
+    #salt=os.urandom(16)
+    salt2=b'\x17JA]C{\x0f2\xecIi\x0c>\xaei\x16'
+    kdf=PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt2,
+        iterations=10**7,
+    )
+    key=base64.urlsafe_b64encode(kdf.derive(password))
+    f=Fernet(key)
+    tmp=input('enter 1 for encryption or enter 2  for decryption :')
+    if tmp=='1':
+        #help for multiline: https://stackoverflow.com/questions/30239092/how-to-get-multiline-input-from-user
+        if fileName == None :
+            print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it. \n")
+            contents = [] 
+            while True:
+                try:
+                    line = input()
+                except EOFError:
+                    break
+                contents.append(line)
+            message = '\n'.join(contents)
+            print('\n')
+            print('encrypted bytes>> \n')
+            print(token)
+        else :
+            message = get_file_content(fileName)
+            token=f.encrypt(message.encode()) 
+            write_file_content(fileName,token.decode()) 
+    if tmp =='2':
+        if fileName==None : 
+            message=input('enter all bytes displayed as string \n')
+        else : 
+            message = get_file_content(fileName)
+        token=f.decrypt(message.encode())
+        if fileName==None : 
+            print('\n resultant text \n')
+            print(token.decode())
+        else : 
+            write_file_content(".temp",token.decode())
+    print('\n\n process is complete \n\n')
+
+def get_file_content(fileName) :
+    fp=open(fileName,"r")
+    content = fp.read()
+    fp.close()
+    return content
+
+def write_file_content(fileName,content):
+    fp=open(fileName,"w")
+    fp.write(content)
+    fp.close()
+
+
+
+if __name__=="__main__" :
+    if len(sys.argv)<2 :
+        main()
+        sys.exit(0)
+    main(fileName=sys.argv[1])
+    sys.exit(0)
--- cryptography/storeTOTP.py
+++ cryptography/storeTOTP.py
@@ -1,6 +1,9 @@
 from getTOTP import getFernet, file
 import json
 import re 
+import sys
+import json
+import os 
 
 def getDict():
     message = open(file,'rb').read()
@@ -20,14 +23,75 @@ def storeDict(newDict):
     return fp
 
 #url format: otpauth://totp/TOTP_INSTANCE_NAME?secret=SECRET_ALPHA_NUMERIC_CHARACTERS&amp;issuer=ISSUER_NAME
-def create_dict_item_from_url(url,providerName):
+def create_dict_item_from_url(url,providerName,printResult=False):
     chrs=['user','secret','issuer']#characteristics(chrs) details to obtain 
-    expression=f'otpauth://totp/(?P<{chrs[0]}>.*)?secret=(?P<{chrs[1]}>.*)&amp;issuer=(?P<{chrs[2]}>.*)$'
+    expression1=f'otpauth://totp/(?P<{chrs[0]}>.*)?secret=(?P<{chrs[1]}>.*)&amp;issuer=(?P<{chrs[2]}>.*)$' #old
+    expression=f'^otpauth://totp/(?P<{chrs[0]}>.*)\?secret=(?P<{chrs[1]}>.*)&issuer=(?P<{chrs[2]}>.*)$' #new
     search=re.search(expression,url)
     result=dict()
-    for tag in chrs : 
-        result[tag]=search.group(tag)
-    return {providerName:result}
+    if search : 
+        for tag in chrs :
+            try :  
+                result[tag]=search.group(tag)
+                if printResult :
+                    print(tag,":",result[tag])
+            except : 
+                print(f"following tag was not captured {tag}")
+        return {providerName:result}
+    return None
 
 def get_new_dict_to_store(old_dict,item_dict):
     return dict(**old_dict,**item_dict)
+
+def write_env_file(content,fileName=".env"):
+    env_file = open(fileName,"w")
+    env_file.write(content)
+    env_file.close()
+
+def read_from_env_file(fileName=".env"):
+    env_file=open(fileName,'r')
+    content=env_file.read()
+    env_file.close()
+    return content
+
+def read_newItem_url(fileName=".env"):
+    content = read_from_env_file(fileName)
+    splits= content.split(':-')
+    return splits[1].strip()
+
+
+def clean_env_variables (fileName=".env"):
+    os.remove(fileName)
+
+
+if __name__=="__main__":
+    if len(sys.argv)<2:
+        print("please provide the command")
+        sys.exit(1)
+    if sys.argv[1]=="printOld":
+        result = getDict()
+        content = json.dumps(result)
+        print(content)
+        sys.exit(0)
+    if sys.argv[1]=="postNewItem":
+        if len (sys.argv)<3 :
+            print("provide 3 arg as provider name" ) 
+            sys.exit(1)
+        content  = read_newItem_url()
+        r=create_dict_item_from_url(content,sys.argv[2],printResult=False)
+        write_env_file(json.dumps(r))
+        sys.exit(0)
+    if sys.argv[1]=="insert":
+        old = getDict()
+        new = json.loads(read_from_env_file())
+        result = get_new_dict_to_store(old,new)
+        write_env_file(json.dumps(result))
+        sys.exit(0)
+    if sys.argv[1]=="store":
+        new = json.loads(read_from_env_file())
+        storeDict(new)
+        sys.exit(0)
+    if sys.argv[1]=="clean":
+        clean_env_variables()
+    print('error, it reached the end but nothing got executed')
+    sys.exit(2)
